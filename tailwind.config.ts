ae80f486bf0d835bbe87c96ca70266fd3b19011d
ChawinNg <ik.chawin@gmail.com>
2025-01-21 15:21:32 +0700

    chores: set up tailwind custom color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,24 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+
+        //For modal
+        "ci-modal-white" : "#F2F9FF",
+        "ci-modal-black" : "#161F3A",
+        "ci-modal-blue" : "#245FA1",
+        "ci-modal-light-blue" : "#006DF5",
+        "ci-modal-cyan-blue" : "#4691A9",
+        "ci-modal-grey" : "#999999",
+        "ci-modal-red" : "#AB3030",
+
+        //For background
+        "ci-bg-dark-blue" : "#1A335F",
+        "ci-bg-blur-white" : "#CDCDCD",
+
+        //For service status
+        "ci-status-green" : "#ADFF89",
+        "ci-status-yellow" : "#FFCC89",
+        "ci-status-red" : "#FF8989",
       },
     },
   },
