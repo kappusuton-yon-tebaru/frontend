5576e2d98987ee77abd2250af7401600d2e0dc6d
Thanawin Suwannasri <papigonwin001@gmail.com>
2025-02-03 11:16:47 +0700

    branch select

--- src/app/repository/[id]/page.tsx
+++ src/app/repository/[id]/page.tsx
@@ -2,22 +2,56 @@ import Image from "next/image";
 
 export default function Repository() {
   const folders = [
-    { name: "Folder 1", date: "31 Feb 2099", time: "14:30", msg: "Commit 1", type: "folder" },
-    { name: "Folder 2", date: "31 Feb 2099", time: "10:00", msg: "Commit 1", type: "folder" },
+    {
+      name: "Folder 1",
+      date: "31 Feb 2099",
+      time: "14:30",
+      msg: "Commit 1",
+      type: "folder",
+    },
+    {
+      name: "Folder 2",
+      date: "31 Feb 2099",
+      time: "10:00",
+      msg: "Commit 1",
+      type: "folder",
+    },
   ];
   const files = [
-    { name: "File 1", date: "31 Feb 2099", time: "14:30", msg: "Commit 1", type: "file" },
-    { name: "File 2", date: "31 Feb 2099", time: "10:00", msg: "Commit 1", type: "file" },
+    {
+      name: "File 1",
+      date: "31 Feb 2099",
+      time: "14:30",
+      msg: "Commit 1",
+      type: "file",
+    },
+    {
+      name: "File 2",
+      date: "31 Feb 2099",
+      time: "10:00",
+      msg: "Commit 1",
+      type: "file",
+    },
   ];
   const itemArr = folders.concat(files);
   return (
     <div>
-      <div className="flex flex-row my-5">
+      <div className="flex flex-row my-5 gap-x-6">
         <h1 className="font-bold text-[24px]">Repositories</h1>
-        <select className="border border-ci-modal-grey px-6 py-1 bg-ci-modal-black rounded-md font-bold">
-          Select Branch
-        </select>
-        <button className="border border-ci-modal-grey px-6 py-1 bg-ci-modal-black rounded-md font-bold">
+        <div className="relative">
+          <Image
+                src={`/git-branch-icon.svg`}
+                alt={`git-branch-icon`}
+                width={24}
+                height={24}
+                className="absolute top-1/2 transform -translate-y-1/2 left-2"
+              />
+          <select className="truncate border border-ci-modal-grey px-7 py-2 bg-ci-modal-black rounded-md font-bold text-white w-full">
+            <option value="1">Option 123456789</option>
+            <option value="2">Option 2</option>
+          </select>
+        </div>
+        <button className="border border-ci-modal-grey px-8 py-1 bg-ci-modal-black rounded-md font-bold">
           Merge
         </button>
       </div>
@@ -37,7 +71,9 @@ export default function Repository() {
               />
               <div className="grid grid-cols-3 w-full">
                 <div className="font-medium">{item.name}</div>
-                <div className="text-ci-modal-grey flex justify-center">{item.msg}</div>
+                <div className="text-ci-modal-grey flex justify-center">
+                  {item.msg}
+                </div>
                 <div className="text-ci-modal-grey flex justify-end">
                   {item.date + ", " + item.time}
                 </div>
