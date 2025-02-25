f48ab8b5f969961f945e4c3a44f6ef1ed1149df7
Punnawatt <punnawathonglerdnapakul@gmail.com>
2025-02-25 22:51:52 +0700

    feat: Rename popup

--- src/components/ProjectSpaceButton.tsx
+++ src/components/ProjectSpaceButton.tsx
@@ -2,16 +2,17 @@ import { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import { Resource } from "@/interfaces/workspace";
 import { useRepositories } from "@/hooks/workspace";
+import RenamePopup from "./RenamePopup";
 
 export default function ProjectSpaceButton({
   projectSpace,
 }: {
   projectSpace: Resource;
 }) {
-  const [option, setOption] = useState(false);
-  const [rename, setRename] = useState(false);
-  const [del, setDel] = useState(false);
-  const [newName, setNewName] = useState(projectSpace.resource_name);
+  const [option, setOption] = useState<boolean>(false);
+  const [rename, setRename] = useState<boolean>(false);
+  const [del, setDel] = useState<boolean>(false);
+  const [newName, setNewName] = useState<string>(projectSpace.resource_name);
   const [repoNum, setRepoNum] = useState<number>(-1);
 
   const dropdownRef = useRef<HTMLDivElement>(null);
@@ -111,43 +112,13 @@ export default function ProjectSpaceButton({
       </div>
 
       {rename && (
-        <div
-          className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 cursor-default"
-          onClick={(e) => e.stopPropagation()}
-        >
-          <div
-            className="flex flex-col bg-ci-modal-dark-blue p-6 rounded-md shadow-lg w-[700px] h-[400px] gap-8 justify-center"
-            ref={renameModalRef}
-          >
-            <div className="flex text-4xl font-bold text-ci-modal-white mb-3 justify-center">
-              Rename Project Space
-            </div>
-            <input
-              type="text"
-              className="mx-16 px-3 py-2 font-medium border border-ci-modal-grey rounded-md focus:outline-none bg-ci-bg-dark-blue"
-              placeholder={projectSpace.resource_name}
-              value={newName}
-              onChange={(e) => setNewName(e.target.value)}
-            />
-            <div className="flex mx-16 mt-4 gap-8">
-              <button
-                className="px-4 py-2 text-white font-semibold bg-ci-bg-dark-blue rounded-md w-full"
-                onClick={() => {
-                  console.log("Renaming to:", newName);
-                  setRename(false);
-                }}
-              >
-                Confirm
-              </button>
-              <button
-                className="px-4 py-2 text-white font-semibold bg-ci-modal-red rounded-md w-full"
-                onClick={() => setRename(false)}
-              >
-                Cancel
-              </button>
-            </div>
-          </div>
-        </div>
+        <RenamePopup
+          renameModalRef={renameModalRef}
+          projectSpace={projectSpace}
+          newName={newName}
+          setNewName={setNewName}
+          setRename={setRename}
+        />
       )}
 
       {del && (
