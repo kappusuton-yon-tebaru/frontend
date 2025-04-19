38fba76a1e086d4ce6c6b96d88bfde4ba4810ebe
Punnawatt <punnawathonglerdnapakul@gmail.com>
2025-04-20 01:36:36 +0700

    feat: connect with github

--- src/app/github/callback/page.tsx
+++ src/app/github/callback/page.tsx
@@ -1,3 +1,14 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Callback() {
-  return <div></div>;
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/");
+  }, []);
+
+  return <div className="flex justify-center items-center">Redirecting...</div>;
 }
--- src/app/github/page.tsx
+++ src/app/github/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { Button } from "antd";
+import Image from "next/image";
+
 export default function Home() {
   function handleLogin() {
     window.location.assign(
@@ -8,8 +11,21 @@ export default function Home() {
   }
 
   return (
-    <div>
-      <button onClick={handleLogin}>Log in with Github</button>
+    <div className="bg-ci-modal-dark-blue flex flex-col justify-center rounded-xl my-24 md:mx-[240px] lg:mx-[420px] gap-4 items-center p-12">
+      <Image
+        src="/github-icon.svg"
+        alt="github-icon"
+        width={100}
+        height={100}
+      />
+      <div className="font-bold text-4xl">Connect with GitHub</div>
+      <div>Connect your account with GitHub to get started</div>
+      <Button
+        className="bg-ci-modal-light-blue border-ci-modal-light-blue text-white w-72 h-12 font-semibold text-lg"
+        onClick={handleLogin}
+      >
+        Connect with GitHub
+      </Button>
     </div>
   );
 }
